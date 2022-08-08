477c176fb539e6bd92746577aba80670278677ab
mystic232508 <110846730+mystic232508@users.noreply.github.com>
2022-08-08 13:21:46 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+let username;
+
+
+document.getElementById("myButton").onclick = function(){
+
+        username = document.getElementById("myText").value;
+        console.log(username);
+        
+}
